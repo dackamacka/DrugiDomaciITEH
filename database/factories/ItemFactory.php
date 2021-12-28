e186514c7a3130f1eec1555b63ef13d119add7da
dackamacka <markovic.jelena060799@gmail.com>
2021-12-28 20:08:28 +0100

    Kreiran model item

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+            'price' => $this->randomFloat(2, 10, 3000),
+            'description' => $this->faker->text()
+        ];
+    }
+}
